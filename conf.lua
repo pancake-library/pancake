bb3900d5308244addaa0f90c217bc278310ffa78
Mighty Pancake <MightyPancake@users.noreply.github.com>
2020-04-09 17:54:21 +0200

    file

---
+++ conf.lua
@@ -0,0 +1,6 @@
+function love.conf (t)
+	t.window.borderless = true -- Remove all border visuals from the window (boolean)
+	t.window.resizable = false -- Let the window be user-resizable (boolean)
+	t.window.fullscreen = true -- Enable fullscreen (boolean)
+	t.window.fullscreentype = "desktop"
+end
